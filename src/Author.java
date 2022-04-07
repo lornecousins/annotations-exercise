44ca5e7f3e8708711a667a08e92b5ab123b86f76
Lorne Cousins <lorne.cousins88@gmail.com>
2022-04-07 10:45:46 -0500

    complete easy fix

--- src/Author.java
+++ src/Author.java
@@ -4,12 +4,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -29,7 +30,7 @@ public class Author extends Person {
     }
 
     @Override
-    public String sortName() {
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
